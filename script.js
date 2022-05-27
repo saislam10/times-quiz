fc5ef7659bae565fd4e0a9cc682c83417bf12c39
Safwan Islam <saislam@davidson.edu>
2022-05-26 19:55:20 -0500

    Populated first page of quiz with answers

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var startBtn = document.querySelector("#start button");
 var nextButton = document.querySelector('#next')
 var timerEL = document.querySelector('#timer');
 var question = document.querySelector('#quiz #question');
-
 var questionNumber = 0;
 var questionsArray = [];
 var secondsLeft = 5;
@@ -22,26 +21,82 @@ function displayState() {
     startEl.style.display = 'none';
     quizEl.style.display = 'block';
     endEl.style.display = 'none';
-
+    displayQuestions();
   }
   if (state === 'end') {
     startEl.style.display = 'none';
     quizEl.style.display = 'none';
     endEl.style.display = 'block';
   }
+}
 
+function displayQuestions(){
+var questionEl = document.querySelector("#question");
+var currentIndex = 0; // current question
+var questions = [
+ {
+   title: "What's 1 + 0?",
+   answers: [
+     "One",
+     "Two", // index 1 of array is the correct answer 
+     "Three",
+     "Four",
+   ],
+   correct: 0 // index of correct answer
+ },
+ {
+   title: "What's 1 + 1?",
+   answers: [
+     "One",
+     "Two", 
+     "Three", 
+     "Four",
+     // index 2 of array is the correct answer 
+   ],
+   correct: 1 // index of correct answer
+ },
+ {
+  title: "What's 1 + 2?",
+  answers: [
+    "One",
+    "Two", // index 1 of array is the correct answer 
+    "Three",
+    "Four",
+  ],
+  correct: 2 // index of correct answer
+},
+{
+  title: "What's 1 + 3?",
+  answers: [
+    "One",
+    "Two", // index 1 of array is the correct answer 
+    "Three",
+    "Four",
+  ],
+  correct: 3 // index of correct answer
+}
+];
+
+var listOfAnswers = questions[currentIndex].answers;
+
+for (var i = 0; i < listOfAnswers.length; i++) {
+  var buttonEl = document.createElement("button");
+  buttonEl.textContent = listOfAnswers[i];
+  questionEl.appendChild(buttonEl);
+}
+}
 //   function questionBank() {
 //     var allQuestions = [
 //       {
-//         question: "What's 1 + 0?",
-//         answers: {
-//           0: "one",
-//           1: "two",
-//           2: "three",
-//           3: "four"
-//         },
-//         correct: 0
-//       },
+      //   question: "What's 1 + 0?",
+      //   answers: {
+      //     0: "one",
+      //     1: "two",
+      //     2: "three",
+      //     3: "four"
+      //   },
+      //   correct: 0
+      // },
 
 //       {
 //         question: "What's 1 + 1?",
@@ -133,12 +188,13 @@ function displayState() {
 
 function init() {
   displayState();
+  setTime();
+  displayQuestions();
 }
 
 startBtn.addEventListener("click", function () {
   state = 'quiz';
   displayState();
-  setTime();
 });
 
 nextButton.addEventListener("click", function () {
